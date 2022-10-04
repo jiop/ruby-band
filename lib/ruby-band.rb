ad8ccdbd515de54128010289213390957c4d3cba
Jérémie IOP <jeremie.iop@ringcentral.com>
2022-10-04 14:05:56 +0200

    attempt 3

--- lib/ruby-band.rb
+++ lib/ruby-band.rb
@@ -4,5 +4,9 @@ require "ruby-band/core"
 require "ruby-band/weka"
 require "ruby-band/apache"
 
+puts "YOLO 1"
+require 'jars/setup'
+puts "YOLO 2"
+
 Random.ancestors[1].instance_eval {remove_const :Random} if defined?(Random)
 java_import 'java.util.Random'
